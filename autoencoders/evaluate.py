3e37d04c371dfcb0e618fe1f5137600ca594e388
Mathieu <mathieu.poiree@polytechnique.edu>
2025-06-02 22:11:47 +0200

    enables different device

--- autoencoders/evaluate.py
+++ autoencoders/evaluate.py
@@ -86,7 +86,7 @@ def main(cfg: DictConfig):
 
     # 4) rebuild the AE model from YAML + load weights
     model_AE = instantiate(cfg.model)
-    state_dict = torch.load(cfg.ckpt_path, map_location="cuda")
+    state_dict = torch.load(cfg.ckpt_path, map_location=cfg.device)
     model_AE.load_state_dict(state_dict)
 
     # 5a) make a hyperdiffusion instance
